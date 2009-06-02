ff559dda037015a582567513e6ef220b0b4f71ba
Billy Cui <billychb@gmail.com>
2009-06-02 17:03:55 +0800

    When switch input mode, also hide the input bar.

--- PinyinIME/src/com/android/inputmethod/pinyin/PinyinIME.java
+++ PinyinIME/src/com/android/inputmethod/pinyin/PinyinIME.java
@@ -267,6 +267,8 @@ public class PinyinIME extends InputMethodService {
             updateIcon(mInputModeSwitcher.switchLanguageWithHkb());
             resetToIdleState(true);
 
+            requestHideSelf(0);
+
             int allMetaState = KeyEvent.META_ALT_ON | KeyEvent.META_ALT_LEFT_ON
                     | KeyEvent.META_ALT_RIGHT_ON | KeyEvent.META_SHIFT_ON
                     | KeyEvent.META_SHIFT_LEFT_ON
